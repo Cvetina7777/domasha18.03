1ec25dc5718ebde652dd56a9b80e47959060b8cc
Cvetina7777 <tt70207252@edu1.pgzaimov.com>
2024-03-19 20:02:53 +0200

    Zadachaa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace zadacha2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vuvedi izrechenie:");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Otpechatvane na izrechenieto bukva po bukva");
+            foreach (char letter in input)
+            {
+                Console.WriteLine(letter);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
